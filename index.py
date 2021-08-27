d061a7b25c91150683ef30eba0381dd051ce31ae
chancew7 <87383530+chancew7@users.noreply.github.com>
2021-08-27 16:17:02 -0400

    Update index.py

--- index.py
+++ index.py
@@ -1,21 +1,16 @@
 import json
 import boto3
-import datetime
-timestamp = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%f')
+
 client = boto3.client('iot-data')
 
-
 def lambda_handler(event, context):
-    destination = event['multiValueQueryStringParameters']['destination'][0]  //replace destination with query string name
+    destination = event['multiValueQueryStringParameters']['destination'][0]
     print(event)
-    roomId = 101                                                                                                                        
 
     response = client.publish(
         topic = 'setDestination',
         qos = 1,
-
-        payload = json.dumps({"location":destination})             //replace destination with query string name
+        payload = json.dumps({"location":destination})
+        #payload = json.dumps(destination) #depends on how we want format of message to be
     )
-    return {
-        "statusCode": 200,
-    } 
+   
